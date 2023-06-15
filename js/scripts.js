331e75bc68579839b9513bfd893ffa7e8e4aa41f
Adam Jones <136383148+adamjoneswork@users.noreply.github.com>
2023-06-15 15:32:01 +0100

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', function() {
+    document.getElementById('loader').style.display = 'none';
+});
+
 document.addEventListener('contextmenu', function (event) {
     if (event.target.closest('#logo-svg')) {
       event.preventDefault();
